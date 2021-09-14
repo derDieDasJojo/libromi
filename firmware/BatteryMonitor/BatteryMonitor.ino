f5abaabb00e6475ca28182f55aa429188e6a5a48
Doug Boari <doug@tastymushroom.com>
2021-09-14 18:42:47 +0200

    Update Battery monitor to use RomiSerial

--- firmware/BatteryMonitor/BatteryMonitor.ino
+++ firmware/BatteryMonitor/BatteryMonitor.ino
@@ -21,24 +21,41 @@
 #include "Arduino.h"
 #include <SoftwareSerial.h>
 #include "VeDirectFrameHandler.h"
+#include <ArduinoSerial.h>
+#include <RomiSerial.h>
 
+using namespace romiserial;
 VeDirectFrameHandler myve;
 
 // SoftwareSerial
 #define rxPin 2                            // RX using Software Serial so we can use the hardware UART to check the ouput
 #define txPin 3                            // TX Not used
-SoftwareSerial veSerial(rxPin, txPin);         
+SoftwareSerial veSerial(rxPin, txPin);
+
+void send_info(IRomiSerial *romiSerial, int16_t *args, const char *string_arg);
+void handle_level_request(IRomiSerial *romiSerial, int16_t *args, const char *string_arg);
+const static MessageHandler handlers[] = {
+        { 'B', 0, false, handle_level_request },
+        { '?', 0, false, send_info },
+};
+
+ArduinoSerial serial(Serial);
+RomiSerial romiSerial(serial, serial, handlers, sizeof(handlers) / sizeof(MessageHandler));
+
+String reply;
 
 void setup() {
 	Serial.begin(115200);                   // output serial port
     veSerial.begin(19200);                  // input serial port (VE device)
     veSerial.flush();
-    Serial.println("DEBUG-setup");
+    // Length From EnvelopeParser
+    reply.reserve(MAX_MESSAGE_LENGTH);
 }
 
 void loop() {    
     ReadVEData();
-    EverySecond();
+    romiSerial.handle_input();
+    delay(1);
 }
 
 void ReadVEData() {
@@ -48,19 +65,19 @@ void ReadVEData() {
     yield();
 }
 
-void EverySecond() {
-    static unsigned long prev_millis;
-
-    if (millis() - prev_millis > 1000) {
-        PrintData();
-        prev_millis = millis();
-    }
+void send_info(IRomiSerial *romiSerial, int16_t *args, const char *string_arg)
+{
+    romiSerial->send("[0,\"BatteryMonitor\",\"0.1\",\"" __DATE__ " " __TIME__ "\"]");
 }
 
-void PrintData() {
+void handle_level_request(IRomiSerial *romiSerial, int16_t *args, const char *string_arg)
+{
+    reply = "[0";
+    
     for ( int i = 0; i < myve.veEnd; i++ ) {
-    Serial.print(myve.veName[i]);
-    Serial.print("= ");
-    Serial.println(myve.veValue[i]);    
+      reply += ",";
+      reply += String(myve.veValue[i]); 
     }
+    reply += "]";
+    romiSerial->send(reply.c_str());
 }
